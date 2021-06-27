b6d38b6be8bedac1f6fd3e5511ac75bfbf52d575
Ramadevi122000 <85234104+Ramadevi122000@users.noreply.github.com>
2021-06-27 12:07:15 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+
+#Whatsapp message automataion in just 2 lines of code!!!!!!!!!!
+import pywhatkit
+from test import number
+pywhatkit.sendwhatmsg(number,'Hello, Good morning',11,58) #minimum there should be 2 min gap
+# number,msg,hrs,min
+
+# Output:
+# Since the screen is not visible I'm copy pasting the output
+In 37 seconds web.whatsapp.com will open and after 20 seconds message will be delivered
+
